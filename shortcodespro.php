661d2f8d69a6da303052483250560fafb3b9f249
sksmatt <sksmatt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-02 01:54:52 +0000

    cleaned license
    git-svn-id: https://plugins.svn.wordpress.org/shortcodes-pro/trunk@418120 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shortcodespro.php
+++ shortcodespro.php
@@ -5,7 +5,23 @@ Version: 1.0.9.9
 Plugin URI: http://www.mattvarone.com/featured-content/shortcodes-pro/
 Description: Shortcodes Pro gives you complete control of your shortcodes and rich editor buttons.
 Author: Matt Varone
-Author URI: http://twitter.com/sksmatt/
+Author URI: http://www.mattvarone.com/
+
+Copyright 2011  (email: contact@mattvarone.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 /**
